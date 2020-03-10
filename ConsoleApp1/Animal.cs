a1a7852e11195a2a7bc4cfc8ba7bd5efcd7b075d
Chris Martin <17U-CMartin@cap.education>
2020-03-10 11:02:35 +0000

    added animal class

---
+++ ConsoleApp1/Animal.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class zoo
+    {
+        public int feedingtime;
+        public enclosure enclosure1;
+        public zoo()
+        {
+        }
+        public void MakeEnclosure()
+        {
+
+        }
+
+        static public void CreateThing(Type type)
+        {
+            var thing = Activator.CreateInstance(type);
+        }
+    
+    }
+    public class enclosure
+    {
+        //enum animals { Zebra, Penguin }
+       public enum animalnames { Jeff, Tom, Joe, Karen, Chris, Steve, Stephen, Oliver, Emily, Bob }
+        public int animalnum;
+        public int size;
+        public float temperature;
+        public float humidity;
+        public string environmentType;
+
+        public List<Animal> storedAnimals;
+
+        public enclosure(string environmentType)
+        {
+            this.environmentType = environmentType;
+            storedAnimals = new List<Animal>();
+        }
+
+        public void addAnimal(Animal animal)
+        {
+
+            storedAnimals.Add(animal);
+           
+
+           
+        }
+
+        public class Animal
+        {
+            public int weight;
+            public int age { get; set; }
+            public int hungry;
+            public string idealenvironment;
+            public string foodtype;
+            public int breedingage;
+            public bool gender;
+            public int happiness;
+            public Animal()
+            {
+                Random random = new Random();
+
+                int weight = random.Next(0, 51);
+                this.weight = weight;
+
+                int age = random.Next(0, 25);
+                this.age = age;
+
+                bool gender = random.Next(0, 2) == 1 ? true : false;
+                this.gender = gender;
+
+                int hungry = random.Next(0, 10);
+                this.hungry = hungry;
+
+                int happiness = random.Next(0, 10);
+                this.happiness = happiness;
+
+            }
+            public void mate()
+            {
+                if (age >= breedingage)
+                {
+                    Console.WriteLine("mate");
+                    if (this.happiness == 9)
+                    {
+                    }
+                    else this.happiness += 1;
+                } 
+                else
+                {
+                    Console.WriteLine("dont mate");
+                }
+            }
+            public void eat()
+            {
+                if (hungry < 9)
+                    Console.WriteLine("eat");
+                else
+                    Console.WriteLine("don't eat");
+            }
+
+
+        }
+        public class Zebra : Animal
+        {
+            public Zebra()
+            {
+                this.idealenvironment = "grass";
+                this.breedingage = 4;
+
+
+            }
+        }
+
+        public class penguin : Animal
+        {
+            public penguin()
+            {
+                this.idealenvironment = "cold water";
+                this.breedingage = 5;
+
+
+            }
+
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ConsoleApp1.enclosure;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,48 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+
+            
+            //zebraEnclosure.environmentType
+
+            Random random = new Random();
+            int animalnamenum = random.Next(0, 10);
+            int animalnum = random.Next(0, 2);
+
+            zoo.CreateThing(animalnamenum.GetType());
+            string animalname = ((enclosure.animalnames)animalnamenum).ToString();
+
+
+            enclosure animalEnclosure; //declaration of enclosure
+
+            
+            switch (animalnum)
+            {
+                case 0:
+                    animalEnclosure = new enclosure("grass"); //instantiation of enclosure
+                    Zebra zebra();
+                       zebra(0) = new Zebra { };
+                    animalEnclosure.addAnimal(zebra(0));
+                    int iamanumber = zebra(0).age;
+                    break;
+                case 1:
+                    animalEnclosure = new enclosure("cold water");
+                    penguin penguin = new penguin { };
+                    animalEnclosure.addAnimal(penguin);
+                    break;
+                    
+            }
+            
+            //searches for 1 female 1 male from a certain species in list
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                zebra(i).age
+            }
         }
     }
 }
